af846b4c354ac68ceeb196f62bcf46dc0cf1498f
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-06 20:50:31 +0100

    init project, everything works

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "GOOGLE_CLIENT_ID": "78038337478-jkv296sf59e57ogibiblsmt5j8pe37mc.apps.googleusercontent.com",
+    "GOOGLE_CLIENT_SECRET": "nQfKoTGK7P7R1G6ZxHGX-Ntq",
+    "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+};
